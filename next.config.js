7c09dc356fc2d38b305254b0821b0cb2fe1b9fb1
Sean Luo <seanluo1@gmail.com>
2025-04-16 01:01:46 -0400

    Remove custom next.config.js paths

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
-const isGithubPages = process.env.NODE_ENV === 'production';
-const repo = 'seanluo1.github.io'; // Your repo name
-
 const nextConfig = {
   output: 'export', // static export 'out/' for gh-pages when running `npm run build`
-  basePath: isGithubPages ? `/${repo}` : '',
-  assetPrefix: isGithubPages ? `/${repo}/` : '',
 };
 
 export default nextConfig;
