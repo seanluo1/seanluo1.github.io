959afbc9dbf8802af3c796a8a1b1940a419f3267
Sean Luo <seanluo1@gmail.com>
2025-04-16 01:32:26 -0400

    fix: Remove use of Optimized Images due to static site restrictions
    feat: Add underline CSS to header

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 const nextConfig = {
   output: 'export', // static export 'out/' for gh-pages when running `npm run build`
+  images: {
+    unoptimized: true // required for static sites
+  }
 };
 
 export default nextConfig;
