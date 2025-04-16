635867deb02e13d218ad84f0ce6980fdaa54df6c
Sean Luo <seanluo1@gmail.com>
2025-04-15 23:03:55 -0400

    Use recommended next.config.js settings

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
+const isGithubPages = process.env.NODE_ENV === 'production';
+const repo = 'seanluo1.github.io'; // Your repo name
+
 const nextConfig = {
   output: 'export', // static export 'out/' for gh-pages when running `npm run build`
-  basePath: '',
+  basePath: isGithubPages ? `/${repo}` : '',
+  assetPrefix: isGithubPages ? `/${repo}/` : '',
 };
 
 export default nextConfig;
